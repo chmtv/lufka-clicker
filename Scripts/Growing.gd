779367dd574712a30f08a2d79657a5956eb9b77a
Korwex Hajaco <damik1318@gmail.com>
2024-04-30 14:38:03 +0200

    growing 2

--- Scripts/Growing.gd
+++ Scripts/Growing.gd
@@ -1,11 +1,18 @@
 extends Panel
 
+# Photosynthesis, the meeting point of the other currencies
+var photosynthesis := 0.5
+# A plant takes 45min to grow fully, 
+var phsPerPhase = 500
+var phsPerSecTotal = 0.6
+
+
 # Light
 var light := 0.0
 var maxLight := 1000.0
 var minLight := 0.0
 var isLighting := false
-var lightPerSec := 1.25
+var lightPerSec := 100.25
 @export var lightProgressBar : TextureProgressBar
 func _on_light_button_down():
 	isLighting = true
@@ -16,7 +23,7 @@ func _on_light_button_up():
 
 # Watering
 @onready var waterTimer := get_tree().create_timer(180)
-var isWatered := false
+var isWatered := true
 func makePlantThirsty():
 	pass
 
@@ -43,15 +50,46 @@ func increaseAirflow():
 var nutrients : int = 0
 @onready var nextNutrientTimer = get_tree().create_timer(30)
 
+
+func getLightCoeff():
+	return 2/(1+exp(0.01*abs(light-500)))
+func getWateringCoeff():
+	return 1.0 if isWatered else 0.25
+func addPhotosynthesis(phs):
+	photosynthesis += phs
+func getPhaseName():
+	var name := ""
+	if photosynthesis < 500:
+		name = "Kiełkowanie"
+	elif photosynthesis < 1000:
+		name = "Wegetacja"
+	elif photosynthesis < 1500:
+		name = "Kwitnienie"
+	elif photosynthesis < 2000:
+		name = "Zbieranie"
+	return name
+
+@export var photosynthesis_label : RichTextLabel
+func getPhotosynthesisCoeffTotal():
+	return getLightCoeff() * (airflowLevel * 0.2) * getWateringCoeff()
+func updatePhsLabels():
+	var text = "[center]"
+	text += '[wave]'+getPhaseName()+'[/wave]'
+	text += "\n"
+	text += "Fotosynteza: x" + str(getPhotosynthesisCoeffTotal()*100) + "%"
+	text += "[center]"
+	photosynthesis_label.text = text
 func _process(delta):
 	# Light decay and increasing
 	if isLighting:
 		light += delta * lightPerSec
 	else:
 		light = max(light - delta * lightPerSec * 0.25, 0.0)
-	print(light)
 	lightProgressBar.value = light
-
+	# Photosynthesis calculation and display
+	var phs = delta * phsPerSecTotal * getPhotosynthesisCoeffTotal()
+	addPhotosynthesis(phs)
+	updatePhsLabels()
 
 func _ready():
 	airflowDecayTimer.connect("timeout", func():
--- Scripts/PlantVisuals.gd
+++ Scripts/PlantVisuals.gd
@@ -1,2 +1,74 @@
-extends TextureRect
+extends Control
 
+# Utility
+var rng = RandomNumberGenerator.new()
+func RandomV2(min, max):
+	rng.randomize()
+	return Vector2(randf_range(min.x,max.x), randf_range(min.y, max.y))
+
+# Array of arrays of tuples of form [vec2 leafPos, float growth]
+class Leaf:
+	var growth = 0.1
+	var subleaves = []
+	var pos
+	func _init(subleaves, pos = Vector2(0.5,0.7), growth = 0.1):
+		self.subleaves = subleaves
+		self.growth = growth
+		self.pos = pos
+	var rng = RandomNumberGenerator.new()
+	func RandomV2(min, max):
+		return Vector2(lerp(min,max, rng.randf())).normalized();
+	func generateSubleaves(amountPerLeaf = 4, generations = 3):
+		for j in range(1,generations):
+			for i in range(1,amountPerLeaf):
+				rng.randomize()
+				var leafPos = Vector2(
+					rng.randf_range(self.pos.x - j*0.15,self.pos.x + j*0.15),
+					rng.randf_range(self.pos.y-0.3,self.pos.y-0.15),
+					)
+				var leafToAdd = Leaf.new([], leafPos)
+				leafToAdd.generateSubleaves(amountPerLeaf, generations - 1)
+				subleaves.append(leafToAdd)
+	func getSubleaves():
+		var result = []
+		for subleaf in self.subleaves:
+			result.append(subleaf)
+			# result.append_array(subleaf.getAllSubleaves())
+		return result
+var Plant = Leaf.new([])
+		
+
+func refreshPlant():
+	pass
+
+func drawSubleafBranches(leaf):
+	for subleaf in leaf.subleaves:
+		pass
+@export var leafTexture : Texture2D
+func _draw():
+	var viewport_size = get_viewport_rect().size
+	draw_set_transform(Vector2.ZERO, 0.0, Vector2(10.0, 10.0))
+
+	# Draw leaves
+	#draw_texture(leafTexture, subleaf.pos)
+	var connectionTuples = [[Plant, Plant.getSubleaves()]]
+	for tuple in connectionTuples:
+		for subleaf in tuple[1]:
+			var root_pos = tuple[0].pos
+			var target_pos = subleaf.pos
+			target_pos.x *= viewport_size.x
+			target_pos.y *= viewport_size.y
+			print(root_pos, target_pos)
+			draw_line(root_pos * viewport_size, target_pos, Color(0.14,0.63,0.4), 3)
+			draw_texture(leafTexture, target_pos-Vector2(16,16))
+			connectionTuples.append([subleaf, subleaf.getSubleaves()])
+			
+			
+	
+			
+func _process(delta):
+	queue_redraw()
+func _ready():
+	Plant.generateSubleaves()
+	
+	print(Plant.subleaves)
