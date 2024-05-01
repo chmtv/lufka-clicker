7767c8dfaa0cb51e39f5a393634c86957ed73f7f
chmtv <damik1318@gmail.com>
2024-05-01 16:27:25 +0200

    growing kinda playable

--- Scripts/Growing.gd
+++ Scripts/Growing.gd
@@ -2,17 +2,18 @@ extends Panel
 
 # Photosynthesis, the meeting point of the other currencies
 var photosynthesis := 0.5
+@export var phsMult : float = 1.0
 # A plant takes 45min to grow fully, 
 var phsPerPhase = 500
 var phsPerSecTotal = 0.6
-
+@export var PlantVisuals : Control
 
 # Light
 var light := 0.0
 var maxLight := 1000.0
 var minLight := 0.0
 var isLighting := false
-var lightPerSec := 100.25
+var lightPerSec := 1.25
 @export var lightProgressBar : TextureProgressBar
 func _on_light_button_down():
 	isLighting = true
@@ -23,9 +24,12 @@ func _on_light_button_up():
 
 # Watering
 @onready var waterTimer := get_tree().create_timer(180)
+@export var waterButton : Button 
 var isWatered := true
 func makePlantThirsty():
-	pass
+	isWatered = false
+	PlantVisuals.isWatered = false
+	waterButton.disabled = false
 
 # Airflow
 var airflowLevel = 0
@@ -55,8 +59,16 @@ func getLightCoeff():
 	return 2/(1+exp(0.01*abs(light-500)))
 func getWateringCoeff():
 	return 1.0 if isWatered else 0.25
+var deltaPhs = 510
 func addPhotosynthesis(phs):
-	photosynthesis += phs
+	var phsToAdd = phs * phsMult
+	photosynthesis += phsToAdd
+	deltaPhs += phsToAdd
+	if photosynthesis > 2000:
+		photosynthesis = phs
+	if deltaPhs > 125:
+		PlantVisuals.refreshPlant(photosynthesis)
+		deltaPhs = 0
 func getPhaseName():
 	var name := ""
 	if photosynthesis < 500:
@@ -68,27 +80,39 @@ func getPhaseName():
 	elif photosynthesis < 2000:
 		name = "Zbieranie"
 	return name
-
+func getPhasePhsProgress(phs = photosynthesis):
+	return int(snapped(fmod(min(phs, 2000) / 500, 4), 1))
 @export var photosynthesis_label : RichTextLabel
+@export var phs_phase_label : RichTextLabel
+@export var phs_progress_bar : TextureProgressBar
 func getPhotosynthesisCoeffTotal():
-	return getLightCoeff() * (airflowLevel * 0.2) * getWateringCoeff()
+	return snapped( getLightCoeff() * (airflowLevel * 0.2) * getWateringCoeff() , 0.001)
+func getPhotosynthesis():
+	return photosynthesis
+func getPhotosynthesisForPhase():
+	var phase_phs = snapped(fmod(photosynthesis, 500.0), 1)
+	phs_progress_bar.value = phase_phs
+	return phase_phs
 func updatePhsLabels():
 	var text = "[center]"
-	text += '[wave]'+getPhaseName()+'[/wave]'
-	text += "\n"
 	text += "Fotosynteza: x" + str(getPhotosynthesisCoeffTotal()*100) + "%"
+	text += "\n"
+	text += "Fotosynteza: " + str(getPhotosynthesisForPhase()) + "/500"
+	text += "\n"
 	text += "[center]"
 	photosynthesis_label.text = text
+	phs_phase_label.text = '[wave][center]'+getPhaseName()+'[/center][/wave]'
 func _process(delta):
 	# Light decay and increasing
 	if isLighting:
 		light += delta * lightPerSec
 	else:
-		light = max(light - delta * lightPerSec * 0.25, 0.0)
+		light = max(light - delta * lightPerSec * 0.125, 0.0)
 	lightProgressBar.value = light
 	# Photosynthesis calculation and display
 	var phs = delta * phsPerSecTotal * getPhotosynthesisCoeffTotal()
 	addPhotosynthesis(phs)
+	
 	updatePhsLabels()
 
 func _ready():
--- Scripts/PlantVisuals.gd
+++ Scripts/PlantVisuals.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var isWatered := true
+
 # Utility
 var rng = RandomNumberGenerator.new()
 func RandomV2(min, max):
@@ -18,17 +20,24 @@ class Leaf:
 	var rng = RandomNumberGenerator.new()
 	func RandomV2(min, max):
 		return Vector2(lerp(min,max, rng.randf())).normalized();
-	func generateSubleaves(amountPerLeaf = 4, generations = 3):
+	func generateSubleaves(amountPerLeaf = 2, generations = 4):
+		self.subleaves = []
 		for j in range(1,generations):
+			var leafPos = Vector2(
+					rng.randf_range(self.pos.x - j*0.1,self.pos.x + j*0.1),
+					rng.randf_range(self.pos.y,self.pos.y-0.25),
+					)
+			var rootStemLeaf = Leaf.new([], leafPos)
+			self.subleaves.append(rootStemLeaf)
 			for i in range(1,amountPerLeaf):
 				rng.randomize()
-				var leafPos = Vector2(
-					rng.randf_range(self.pos.x - j*0.15,self.pos.x + j*0.15),
-					rng.randf_range(self.pos.y-0.3,self.pos.y-0.15),
+				leafPos = Vector2(
+					rng.randf_range(self.pos.x - i*0.25,self.pos.x + i*0.25),
+					rng.randf_range(self.pos.y,self.pos.y-0.25),
 					)
-				var leafToAdd = Leaf.new([], leafPos)
+				var leafToAdd = Leaf.new([rootStemLeaf], leafPos)
 				leafToAdd.generateSubleaves(amountPerLeaf, generations - 1)
-				subleaves.append(leafToAdd)
+				self.subleaves.append(leafToAdd)
 	func getSubleaves():
 		var result = []
 		for subleaf in self.subleaves:
@@ -37,17 +46,25 @@ class Leaf:
 		return result
 var Plant = Leaf.new([])
 		
-
-func refreshPlant():
-	pass
-
+func getPhaseLevel(phs):
+	return int(snapped(fmod(min(phs, 2100) / 500, 4), 1))
+func refreshPlant(phs : float):
+	# phs is a 0-2000 float
+	print("jebany phs to:", phs)
+	var leafs = 3 if phs > 1500 else 2
+	var generations = 1 + getPhaseLevel(phs)
+	print(getPhaseLevel(phs))
+	
+	Plant.generateSubleaves(leafs, generations)
+	print(generations, leafs, "gowno chuj jebac")
+	queue_redraw()
 func drawSubleafBranches(leaf):
 	for subleaf in leaf.subleaves:
 		pass
 @export var leafTexture : Texture2D
 func _draw():
-	var viewport_size = get_viewport_rect().size
-	draw_set_transform(Vector2.ZERO, 0.0, Vector2(10.0, 10.0))
+	var viewport_size = get_rect().size
+	# draw_set_transform(Vector2.ZERO, 0.0, Vector2(10.0, 10.0))
 
 	# Draw leaves
 	#draw_texture(leafTexture, subleaf.pos)
@@ -58,8 +75,8 @@ func _draw():
 			var target_pos = subleaf.pos
 			target_pos.x *= viewport_size.x
 			target_pos.y *= viewport_size.y
-			print(root_pos, target_pos)
 			draw_line(root_pos * viewport_size, target_pos, Color(0.14,0.63,0.4), 3)
+			# draw_line(Vector2(0,0), Vector2(10,10), Color(1,1,1), 5)
 			draw_texture(leafTexture, target_pos-Vector2(16,16))
 			connectionTuples.append([subleaf, subleaf.getSubleaves()])
 			
@@ -67,7 +84,7 @@ func _draw():
 	
 			
 func _process(delta):
-	queue_redraw()
+	pass
 func _ready():
 	Plant.generateSubleaves()
 	
